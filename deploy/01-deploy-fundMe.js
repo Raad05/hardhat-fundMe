848ea0443b8675b3cfadf785b9e0589b955a713a
Raad05 <ashabulraad@gmail.com>
2024-02-19 18:56:06 +0600

    added 01-deploy-fundMe

--- deploy/01-deploy-fundMe.js
+++ deploy/01-deploy-fundMe.js
@@ -1,4 +1,5 @@
 const { network } = require("hardhat");
+const { networkConfig } = require("../helper-hardhat-config");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
@@ -8,11 +9,15 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   // if chainId is X use address Y
   // if chainId is Z use address A
 
+  const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+
+  // if the contract doesn't exist, we deploy a minimal version for our local testing
+
   // what happens when we want to change chains?
   // when going for localhost or hardhat network we want to use a mock
   const fundMe = await deploy("fundMe", {
     from: deployer,
-    args: [], // put priceFeed address
+    args: [ethUsdPriceFeedAddress], // put priceFeed address
     log: true,
   });
 };
