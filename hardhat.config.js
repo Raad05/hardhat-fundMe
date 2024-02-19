570d36f7095aeb8daad43a77cabac2d8fa747800
Raad05 <ashabulraad@gmail.com>
2024-02-19 19:13:09 +0600

    added MockV3Aggregator.sol

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,7 +11,16 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY || "PRIVATE_KEY";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "ETHERSCAN_API";
 
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+      },
+      {
+        version: "0.6.6",
+      },
+    ],
+  },
   networks: {
     sepolia: {
       url: SEPOLIA_RPC,
