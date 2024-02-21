c9d7b5536f7664eaa9a99442a8daa619e354e0fa
Raad05 <ashabulraad@gmail.com>
2024-02-21 12:12:57 +0600

    added sendFund function tests

--- test/unit/fundMe.test.js
+++ test/unit/fundMe.test.js
@@ -1,10 +1,12 @@
 const { getNamedAccounts, ethers, deployments } = require("hardhat");
-const { assert } = require("chai");
+const { assert, expect } = require("chai");
 
 describe("FundMe", async function () {
   let contract;
   let deployer;
   let mockV3Aggregator;
+  const sendValue = ethers.parseEther("1");
+
   beforeEach(async function () {
     deployer = (await getNamedAccounts()).deployer;
     await deployments.fixture(["all"]);
@@ -16,6 +18,24 @@ describe("FundMe", async function () {
     it("sets the aggregator addresses correctly", async function () {
       const res = await contract.priceFeed();
       assert.equal(res, mockV3Aggregator.target);
+    });
+  });
+
+  describe("sendFund", async function () {
+    it("fails if you don't send enough ETH", async function () {
+      await expect(contract.sendFund()).to.be.revertedWith("Insufficient fund");
+    });
+
+    it("updates the amount funded data structure", async function () {
+      await contract.sendFund({ value: sendValue });
+      const res = await contract.addressToAmountFunded(deployer);
+      assert.equal(res.toString(), sendValue.toString());
+    });
+
+    it("adds funders to array of funders", async function () {
+      await contract.sendFund({ value: sendValue });
+      const funder = await contract.funders(0);
+      assert.equal(funder, deployer);
     });
   });
 });
