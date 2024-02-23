77849ac11a2f4d486ea318b271f84d4ea8c48b0b
Raad05 <ashabulraad@gmail.com>
2024-02-23 18:19:07 +0600

    gas optimization with cheaperWithdrawFund

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -21,8 +21,8 @@ contract FundMe {
     // gas optimization
     uint public constant MIN_USD = 20 * 1e18;
 
-    address[] public funders;
-    mapping(address => uint) public addressToAmountFunded;
+    address[] public s_funders;
+    mapping(address => uint) public s_addressToAmountFunded;
 
     function sendFund() public payable {
         require(
@@ -30,16 +30,16 @@ contract FundMe {
             "Insufficient fund"
         );
         // add funder
-        funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] += msg.value;
+        s_funders.push(msg.sender);
+        s_addressToAmountFunded[msg.sender] += msg.value;
     }
 
     function withdrawFund() public onlyOwner {
-        for (uint i = 0; i < funders.length; i++) {
-            address funder = funders[i];
-            addressToAmountFunded[funder] = 0;
+        for (uint i = 0; i < s_funders.length; i++) {
+            address funder = s_funders[i];
+            s_addressToAmountFunded[funder] = 0;
         }
-        funders = new address[](0);
+        s_funders = new address[](0);
 
         (bool sent, ) = payable(msg.sender).call{value: address(this).balance}(
             ""
@@ -47,12 +47,25 @@ contract FundMe {
         require(sent, "Failed to transact ETH");
     }
 
-    modifier onlyOwner() {
+    modifier onlyOwner {
         // require(msg.sender == i_owner, "You are not the i_owner");
         if (msg.sender != i_owner) {
             revert NotOwner();
         }
         _;
+    }
+
+    function cheaperWithdrawFund() public payable onlyOwner {
+        address[] memory funders = s_funders;
+
+        for(uint i = 0; i < funders.length; i++) {
+            address funder = funders[i];
+            s_addressToAmountFunded[funder] = 0;
+        }
+
+        s_funders = new address[](0);
+        (bool sent, ) = i_owner.call{value: address(this).balance}("");
+        require(sent);
     }
 
     receive() external payable {
